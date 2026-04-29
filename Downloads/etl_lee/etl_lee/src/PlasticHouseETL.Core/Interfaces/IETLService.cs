b288d67c5daaf44fab1a78aca0be48c383b232ac
StevenLee77970 <leeg77970@gmail.com>
2026-04-29 11:05:23 -0600

    Delete Downloads/etl_lee/etl_lee directory

--- Downloads/etl_lee/etl_lee/src/PlasticHouseETL.Core/Interfaces/IETLService.cs
+++
@@ -1,9 +0,0 @@
-namespace PlasticHouseETL.Core.Interfaces;
-
-public interface IETLService
-{
-    Task ExecuteAsync(DateTime fechaInicio, DateTime fechaFin);
-    Task ExecuteFullAsync();
-    Task ExecuteIncrementalAsync();
-}
-
--- Downloads/etl_lee/etl_lee/src/PlasticHouseETL.Shared/DTOs/DimensionDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace PlasticHouseETL.Shared.DTOs;
-
-public class DimensionDTO
-{
-    public int Id { get; set; }
-    public string? Nombre { get; set; }
-}
-
